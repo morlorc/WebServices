a0bffe85ddc5db0f8c6b184702dace56da5639ca
morlorc <pelissier.sylvain@hotmail.fr>
2018-05-15 16:39:46 +0200

    Java Beans

---
+++ WebServices/src/models/Association.java
@@ -0,0 +1,42 @@
+package models;
+
+public class Association {
+
+	private String nom;
+	private String mail;
+	private String id_asso;
+	private String siren;
+	private String mdp;
+	 
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getId_asso() {
+		return id_asso;
+	}
+	public void setId_asso(String id_asso) {
+		this.id_asso = id_asso;
+	}
+	public String getSiren() {
+		return siren;
+	}
+	public void setSiren(String siren) {
+		this.siren = siren;
+	}
+	public String getMdp() {
+		return mdp;
+	}
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+	
+}
---
+++ WebServices/src/models/Benevole.java
@@ -0,0 +1,61 @@
+package models;
+
+public class Benevole {
+
+	private int id_bv;
+	private String prenom;
+	private String nom;
+	private int age;
+	private String mail;
+	private String mdp;
+	
+	public int getId_bv() {
+		return id_bv;
+	}
+	
+	public void setId_bv(int id_bv) {
+		this.id_bv = id_bv;
+	}
+	
+	public String getPrenom() {
+		return prenom;
+	}
+	
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getMail() {
+		return mail;
+	}
+	
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	public String getMdp() {
+		return mdp;
+	}
+	
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+	
+	
+}
