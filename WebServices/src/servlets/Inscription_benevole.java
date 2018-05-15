58f66249e1a0318c5fa979c80a3b797bacdd3d80
morlorc <pelissier.sylvain@hotmail.fr>
2018-05-15 12:19:11 +0200

    Ajout conditions vals/longueurs max

--- WebServices/src/servlets/Inscription_benevole.java
+++ WebServices/src/servlets/Inscription_benevole.java
@@ -39,17 +39,27 @@ public class Inscription_benevole extends HttpServlet {
     	    if ( nom == null || nom.trim().length() < 3 ) {
     	        throw new Exception( "Le nom doit contenir au moins 3 caractères." );
     	    }
+    	    else if (nom.trim().length() > 29 ) {
+    	        throw new Exception( "Le nom doit contenir moins de 30 caractères." );
+    	    }
     	}
     	
     	private void validationPrenom( String prenom ) throws Exception{
     	    if ( prenom == null || prenom.trim().length() < 3 ) {
     	        throw new Exception( "Le prénom doit contenir au moins 3 caractères." );
-    	    }
+    	    	}
+        	    else if (prenom.trim().length() > 29 ) {
+        	        throw new Exception( "Le prénom doit contenir moins de 30 caractères." );
+        	    }
+    	        
     	}
     	
         private void validationAge( int age ) throws Exception{
     	    if ( age < 16) {
     	    	throw new Exception( "Vous devez avoir au moins 16 ans." );
+    	    }
+    	    else if (age > 130) {
+    	    	throw new Exception( "Vous avez saisi un age trop grand" );
     	    }
         }
         
@@ -61,6 +71,9 @@ public class Inscription_benevole extends HttpServlet {
             } else {
                 throw new Exception( "Merci de saisir une adresse mail." );
             }
+            if ( email.trim().length() > 100 ) {
+                throw new Exception( "L'addresse mail est trop longue" ); 
+            }
         }
         
         private void validationMotsDePasse( String motDePasse ) throws Exception{
@@ -71,5 +84,8 @@ public class Inscription_benevole extends HttpServlet {
             } else {
                 throw new Exception("Merci de saisir votre mot de passe.");
             }
+    	    if (motDePasse.trim().length() > 100 ) {
+    	        throw new Exception( "Le mot de passe ne peut dépasser 100 caaractères" );
+    	    }
         }
 }
