2aa1529a4194437900fe96d63e05a87bb7311a7a
tom <tom@symfony.fr>
2018-05-17 16:47:52 +0200

    inscription avec verifiction champs

--- WebServices/src/controller/Inscription_association.java
+++ WebServices/src/controller/Inscription_association.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,9 +18,14 @@ public class Inscription_association extends HttpServlet {
     public static final String CHAMP_SIREN_AS = "SIREN";
     public static final String CHAMP_EMAIL_AS = "mailA";
     public static final String CHAMP_PASS_AS = "mdpA";
+    public static final String ATT_ERREURS  = "erreurs";
+    public static final String ATT_RESULTAT = "resultat";
 
 	public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 		
+		String resultat;
+        Map<String, String> erreurs = new HashMap<String, String>();
+        
         String nom = request.getParameter(CHAMP_NOM_AS);
         String siren = request.getParameter(CHAMP_SIREN_AS);
         String email = request.getParameter(CHAMP_EMAIL_AS);
@@ -26,12 +33,46 @@ public class Inscription_association extends HttpServlet {
         
         try {
         	validationNom( nom );
+            
+        } catch(Exception e) {
+        	erreurs.put(CHAMP_NOM_AS, e.getMessage());
+        	
+        }
+        
+        try {
             validationSiren( siren );
+          
+        } catch(Exception e) {
+        	erreurs.put(CHAMP_SIREN_AS, e.getMessage());
+        	
+        }
+        
+        try {
+
             validationEmail( email );
+        } catch(Exception e) {
+
+        	erreurs.put(CHAMP_EMAIL_AS, e.getMessage());
+        }
+        
+        try {
+ 
             validationMotsDePasse( motDePasse );
         } catch(Exception e) {
-        	System.out.println("Paramtres invalides");
+
+        	erreurs.put(CHAMP_PASS_AS, e.getMessage());
         }
+        
+        if ( erreurs.isEmpty() ) {
+            resultat = "Succès de l'inscription.";
+        } else {
+            resultat = "Échec de l'inscription.";
+        }
+        
+        request.setAttribute( ATT_ERREURS, erreurs );
+        request.setAttribute( ATT_RESULTAT, resultat );
+        
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
       	
 	}
 	
