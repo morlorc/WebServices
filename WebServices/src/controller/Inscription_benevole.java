80af3ad3e8c7ff7240eaf4f1aba40487f6948c39
morlorc <pelissier.sylvain@hotmail.fr>
2018-05-17 16:28:03 +0200

    Modif lien

--- WebServices/src/controller/Inscription_benevole.java
+++ WebServices/src/controller/Inscription_benevole.java
@@ -5,9 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/inscription_benevole")
 
 public class Inscription_benevole extends HttpServlet {
 
