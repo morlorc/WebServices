ecf3c73c912c4329e3645508a897c634270fb28c
Pierre <Pierre@10.5.5.178>
2018-05-17 17:16:40 +0200

    Ajout de sauts de ligne dans les message d'erreur d'inscription bénévole

--- WebServices/src/controller/Inscription_benevole.java
+++ WebServices/src/controller/Inscription_benevole.java
@@ -88,19 +88,19 @@ public class Inscription_benevole extends HttpServlet {
 
 	private void validationNom( String nom ) throws Exception{
 	    if ( nom == null || nom.trim().length() < 3 ) {
-	        throw new Exception( "Le nom doit contenir au moins 3 caractères." );
+	        throw new Exception( "Le nom doit contenir au moins 3 caractères.<br />" );
 	    }
 	    else if (nom.trim().length() > 29 ) {
-	        throw new Exception( "Le nom doit contenir moins de 30 caractères." );
+	        throw new Exception( "Le nom doit contenir moins de 30 caractères.<br />" );
 	    }
 	}
 	
 	private void validationPrenom( String prenom ) throws Exception{
 	    if ( prenom == null || prenom.trim().length() < 3 ) {
-	        throw new Exception( "Le prénom doit contenir au moins 3 caractères." );
+	        throw new Exception( "Le prénom doit contenir au moins 3 caractères.<br />" );
 	    	}
 	    else if (prenom.trim().length() > 29 ) {
-	        throw new Exception( "Le prénom doit contenir moins de 30 caractères." );
+	        throw new Exception( "Le prénom doit contenir moins de 30 caractères.<br />" );
 	    }
 	}
 	
@@ -108,39 +108,39 @@ public class Inscription_benevole extends HttpServlet {
     	if (age != "") {
     		int age_i = Integer.parseInt(age);
 		    if ( age_i < 16) {
-		    	throw new Exception( "Vous devez avoir au moins 16 ans." );
+		    	throw new Exception( "Vous devez avoir au moins 16 ans.<br />" );
 		    }
 		    else if (age_i > 130) {
-		    	throw new Exception( "Vous avez saisi un age trop grand" );
+		    	throw new Exception( "Vous avez saisi un age trop grand.<br />" );
 		    }
     	}else {
-    		throw new Exception("Veuillez saisir un âge.");
+    		throw new Exception("Veuillez saisir un âge.<br />");
     	}
     }
     
     private void validationEmail( String email ) throws Exception{
         if ( email != null && email.trim().length() != 0 ) {
             if ( !email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) {
-                throw new Exception( "Merci de saisir une adresse mail valide." );
+                throw new Exception( "Merci de saisir une adresse mail valide.<br />" );
             }
         } else {
-            throw new Exception( "Merci de saisir une adresse mail." );
+            throw new Exception( "Merci de saisir une adresse mail.<br />" );
         }
         if ( email.trim().length() > 100 ) {
-            throw new Exception( "L'addresse mail est trop longue" ); 
+            throw new Exception( "L'addresse mail est trop longue.<br />" ); 
         }
     }
     
     private void validationMotsDePasse( String motDePasse ) throws Exception{
     	if (motDePasse != null && motDePasse.trim().length() != 0) {
     		if (motDePasse.trim().length() < 8) {
-                throw new Exception("Le mot de passe doit contenir au moins 8 caractères.");
+                throw new Exception("Le mot de passe doit contenir au moins 8 caractères.<br />");
             }
         } else {
-            throw new Exception("Merci de saisir votre mot de passe.");
+            throw new Exception("Merci de saisir votre mot de passe.<br />");
         }
 	    if (motDePasse.trim().length() > 100 ) {
-	        throw new Exception( "Le mot de passe ne peut dépasser 100 caractères" );
+	        throw new Exception( "Le mot de passe ne peut dépasser 100 caractères.<br />" );
 	    }
     }
 }
