d32745decebb68a97d9f2194d11517713887672b
morlorc <pelissier.sylvain@hotmail.fr>
2018-05-24 14:34:12 +0200

    mcv servlet test

--- WebServices/src/controller/Connexion_benevole.java
+++ WebServices/src/controller/Connexion_benevole.java
@@ -1,12 +1,7 @@
 package controller;
 
-import util.Util_Connexion;
-import util.manipulation_xml.Personnes_xml;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,60 +9,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import forms.Connexion_benevole_form;
+import models.personnes.Personne;
+
 @WebServlet(urlPatterns = "/connexion_benevole")
 
 public class Connexion_benevole extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	
+
+    public static final String ATT_PERSONNE = "personne";
+    public static final String ATT_FORM = "form";
 	public static final String VUE = "/WEB-INF/ConnectionBenevole.jsp";
-	public static final String XML = "D:\\Users\\Pierre\\Desktop\\WebsGit\\WebServices\\WebServices\\WebContent\\personnes.xml";
-    public static final String CHAMP_EMAIL_BN = "mailC";
-    public static final String CHAMP_PASS_BN = "mdpC";
-    public static final String ATT_ERREURS  = "erreurs";
-    public static final String ATT_RESULTAT = "resultat";
-
+		
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+        /* Affichage de la page d'inscription */
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
+	
     public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-
-    	String resultat;
-        Map<String, String> erreurs = new HashMap<String, String>();
-
-        String email = request.getParameter( CHAMP_EMAIL_BN );
-        String motDePasse = request.getParameter( CHAMP_PASS_BN );
-      	
-        if (Personnes_xml.authentificationValide( email, motDePasse, new File (XML)) ) {
-        	System.out.println("Connexion OK");
-        	//Reste à compléter
-        } else {
-        	System.out.println("Echec connexion");
-        	try {
-        		Util_Connexion.mailVide(email);
-        	} catch (Exception e) {
-        		System.out.println("Mail vide");
-        		erreurs.put(CHAMP_EMAIL_BN, e.getMessage());
-        	}
-        	
-        	try {
-        		Util_Connexion.mdpVide(motDePasse);
-        	} catch (Exception e) {
-        		System.out.println("Mdp vide");
-        		erreurs.put(CHAMP_PASS_BN, e.getMessage());
-        	}
-        	
-        	if (erreurs.isEmpty()) {
-        		erreurs.put(CHAMP_EMAIL_BN, "Ce compte n'existe pas.");
-        	}
-        }
-
-        if ( erreurs.isEmpty() ) {
-            resultat = "Succès de la connexion.";
-        } else {
-            resultat = "Échec de la connexion.";
-        }
-        
-        request.setAttribute( ATT_ERREURS, erreurs );
-        request.setAttribute( ATT_RESULTAT, resultat );
-        
+        /* Préparation de l'objet formulaire */
+        Connexion_benevole_form form = new Connexion_benevole_form();
+		
+        /* Appel au traitement et à la validation de la requête, et récupération du bean en résultant */
+        Personne personne = form.connecterPersonne( request );
+		
+        /* Stockage du formulaire et du bean dans l'objet request */
+        request.setAttribute( ATT_FORM, form );
+        request.setAttribute( ATT_PERSONNE, personne );
+		
         this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 }
