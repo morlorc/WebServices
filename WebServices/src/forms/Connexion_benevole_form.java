373eefbdb7c3cb3c33565a8c67dc0824f4f86b85
morlorc <pelissier.sylvain@hotmail.fr>
2018-05-24 13:39:28 +0200

    form try

---
+++ WebServices/src/forms/Connexion_benevole_form.java
@@ -0,0 +1,99 @@
+package forms;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+import models.personnes.Personne;
+import models.personnes.Personnes;
+import util.Util_Connexion;
+
+public final class Connexion_benevole_form {
+
+	private static final String CHAMP_EMAIL_BN = "mailC";
+	private static final String CHAMP_PASS_BN = "mdpC";
+	private Map<String, String> ATT_ERREURS = new HashMap<String, String>();
+	private static String ATT_RESULTAT = "resultat";
+	/* private static final String USERS_PATH = "/WEB-INF/database/utilisateurs.xml";*/
+
+	public String getResultat() {
+		return ATT_RESULTAT;
+	}
+
+	public Map<String, String> getErreurs() {
+		return ATT_ERREURS;
+	}
+
+	public Personne connecterPersonne(HttpServletRequest request) {
+		/* Récupération des champs du formulaire */
+		String email = getValeurChamp(request, CHAMP_EMAIL_BN);
+		String motDePasse = getValeurChamp(request, CHAMP_PASS_BN);
+
+		Personne personne = new Personne();
+
+		/* Validation du champ email. */
+		try {
+			Util_Connexion.mailVide(email);
+		} catch (Exception e) {
+			setErreur(CHAMP_EMAIL_BN, e.getMessage());
+		}
+		personne.setMail_pers(email);
+
+		/* Validation du champ mot de passe. */
+		try {
+			Util_Connexion.mdpVide(motDePasse);
+		} catch (Exception e) {
+			setErreur(CHAMP_PASS_BN, e.getMessage());
+		}
+		personne.setMdp_pers(motDePasse);
+
+		/* Initialisation du résultat global de la validation. */
+		if (ATT_ERREURS.isEmpty()) {
+			try {
+				personne = verifierLogins(email, motDePasse, request);
+				ATT_RESULTAT = "Succès de la connexion.";
+			} catch (Exception e) {
+				setErreur("wrongCredentials", e.getMessage());
+				ATT_RESULTAT = "échec de la connexion.";
+			}
+		} else {
+			ATT_RESULTAT = "échec de la connexion.";
+		}
+
+		return personne;
+	}
+
+	private Personne verifierLogins(String email, String motDePasse, HttpServletRequest request) throws Exception {
+		JAXBContext jaxbContext = JAXBContext.newInstance(Personnes.class);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		Personnes listPersonnes = (Personnes) jaxbUnmarshaller
+				.unmarshal(new File(request.getServletContext().getRealPath(USERS_PATH)));
+		for (Personne u : listPersonnes.getPersonne()) {
+			if (u.getMail_pers().equals(email) && u.getMdp_pers().equals(motDePasse))
+				return u;
+		}
+		throw new Exception("Aucun bénévole n'est enregistré avec cet email et ce mot de passe");
+	}
+	/*
+	 * Ajoute un message correspondant au champ sp�cifi� � la map des erreurs.
+	 */
+	private void setErreur(String champ, String message) {
+		ATT_ERREURS.put(champ, message);
+	}
+
+	/*
+	 * M�thode utilitaire qui retourne null si un champ est vide, et son contenu
+	 * sinon.
+	 */
+	private static String getValeurChamp(HttpServletRequest request, String nomChamp) {
+		String valeur = request.getParameter(nomChamp);
+		if (valeur == null || valeur.trim().length() == 0) {
+			return null;
+		} else {
+			return valeur;
+		}
+	}
+}
