d683019a52a57008e5b138bfeba42a9771f38fd8
Pierre <Pierre@LAPTOP-00O4DRT2>
2018-05-26 23:01:53 +0200

    Résolution partielle du bug - Ne fonctionne toujours pas complètement

--- WebServices/src/models/associations/Associations.java
+++ WebServices/src/models/associations/Associations.java
@@ -3,8 +3,10 @@ package models.associations;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 @XmlRootElement
+@XmlTransient
 
 public class Associations {
 	
